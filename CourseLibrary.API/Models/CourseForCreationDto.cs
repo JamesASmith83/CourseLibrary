81bf972383557e051fcee87a9fe68600c2783d38
James Smith <james.a.smith.83@hotmail.co.uk>
2019-12-08 12:38:33 +0000

    valiation changes

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,8 +1,17 @@
-﻿namespace CourseLibrary.API.Models
+﻿using CourseLibrary.API.ValidationAttributes;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CourseLibrary.API.Models
 {
-    public class CourseForCreationDto
+    [CourseTitleMustBeDifferentFromDescription(ErrorMessage = "Title must be different from description")]
+    public class CourseForCreationDto 
     {
+        [Required(ErrorMessage = "You should fill out a title")]
+        [MaxLength(100, ErrorMessage = "The title shouldn't be more than 100 characters")]
         public string Title { get; set; }
-        public string Description { get; set; }    
+
+        [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters")]
+        public string Description { get; set; }
     }
 }
